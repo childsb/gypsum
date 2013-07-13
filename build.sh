2ea99a840f6c3f764865db4d66499025b875605a
childsb <bchilds@redhat.com>
2013-07-13 13:06:33 -0500

    clean up build, add linux support

--- build.sh
+++ build.sh
@@ -1,15 +1,39 @@
 #! /bin/sh
 
 osxBuild(){
+	unamestr=`uname`
     echo "Running osx build..."
 	# INCLUDEPATH=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers
     # COPTS="-D_SAFE -std=c99 -fnested-functions "
-    mkdir lib
-    libName="posix_Process_osx_64.jnilib"
-	gcc -I/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/ -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers -c src/main/c/posix_Process.c
+    #libName=posix_Process.jnilib  // this is for native library loading and not absolute. 
+    libName="posix_Process.so"
+    dest="lib/${unamestr}"
+    mkdir -p $dest
+    
+    gcc -I/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/ -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers -c src/main/c/posix_Process.c
 	gcc -dynamiclib -o $libName posix_Process.o
-	mv ${libName} lib/
+	mv ${libName} ${dest}
 }
 
-osxBuild
+linuxBuild(){
+ unamestr=`uname`
+ echo "Running Linux build..."
+ INCLUDEPATH="${JAVA_HOME}/include"
+ libName="posix_Process.so"
+ dest="lib/${unamestr}"
+ mkdir -p $dest
+ gcc -I$INCLUDEPATH -c src/main/c/posix_Process.c
+ gcc -o $libName} posix_Process.o
+ mv ${libName} ${dest}
+ 
+}
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+  linuxBuild
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  osxBuild
+fi
+
+
 
